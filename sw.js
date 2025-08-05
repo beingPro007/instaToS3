61334d4205cca752fd0565b90d79b00a21b01d71
Gautam Rana <rdxtreme@proton.me>
2025-08-05 13:56:16 +0530

    feat: Add pwa

---
+++ sw.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = 'instas3-cache-v1';
+
+// This list now includes the manifest and both icons
+const FILES_TO_CACHE = [
+  '/',
+  'index.html',
+  'manifest.json',
+  'images/icon-192x192.png',
+  'images/icon-512x512.png'
+];
+
+self.addEventListener('install', (event) => {
+  console.log('[Service Worker] Install');
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('[Service Worker] Caching app shell');
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('[Service Worker] Activate');
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== CACHE_NAME) {
+          console.log('[Service Worker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.url.includes('onrender.com')) {
+    return event.respondWith(fetch(event.request));
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
