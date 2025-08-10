b1fe6cc37f9ae699d69f11ee92b3e985f164bf6a
Gautam Rana <rdxtreme@proton.me>
2025-08-10 07:40:01 +0530

    ui changes

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 import express from "express";
 import { configDotenv } from "dotenv";
-import cors from "cors";    
+import cors from "cors";
 
 configDotenv();
 
 const app = express();
 
+const isProd = process.env.NODE_ENV === "production";
+
 app.use(cors({
-    origin: "*"
-}))
+  origin: isProd
+    ? process.env.CORS_ORIGIN || "*"
+    : "*",
+}));
 
 export default app;
